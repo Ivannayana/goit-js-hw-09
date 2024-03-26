89320712f2c113615fdd14a5f7dd5c71c01651ff
Ivannayana <151822259+Ivannayana@users.noreply.github.com>
2024-03-26 21:26:19 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,37 +1,40 @@
-const form = document.querySelector('.feedback-form');
-const emailInput = form.querySelector('input[name="email"]');
-const messageInput = form.querySelector('textarea[name="message"]');
-
-const feedbackKey = 'feedback-form-state';
-
-function saveFeedbackToLocalStorage() {
-  const feedback = {
-    email: emailInput.value,
-    message: messageInput.value,
+function saveToLocalStorage() {
+  const formData = {
+    email: document.querySelector('input[name="email"]').value.trim(),
+    message: document.querySelector('textarea[name="message"]').value.trim(),
   };
-  localStorage.setItem(feedbackKey, JSON.stringify(feedback));
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-function loadFeedbackFromLocalStorage() {
-  const savedFeedback = localStorage.getItem(feedbackKey);
-  if (savedFeedback) {
-    const { email, message } = JSON.parse(savedFeedback);
-    emailInput.value = email;
-    messageInput.value = message;
+function fillFormFromLocalStorage() {
+  const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (formData) {
+    document.querySelector('input[name="email"]').value = formData.email;
+    document.querySelector('textarea[name="message"]').value = formData.message;
   }
 }
 
 function clearLocalStorage() {
-  localStorage.removeItem(feedbackKey);
+  localStorage.removeItem('feedback-form-state');
 }
 
-form.addEventListener('input', saveFeedbackToLocalStorage);
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  console.log('Form submitted!');
-  clearLocalStorage();
-  emailInput.value = '';
-  messageInput.value = '';
+fillFormFromLocalStorage();
+
+document.querySelector('.feedback-form').addEventListener('input', () => {
+  saveToLocalStorage();
 });
 
-window.addEventListener('load', loadFeedbackFromLocalStorage);
+document.querySelector('.feedback-form').addEventListener('submit', event => {
+  event.preventDefault();
+  const email = document.querySelector('input[name="email"]').value.trim();
+  const message = document
+    .querySelector('textarea[name="message"]')
+    .value.trim();
+
+  if (email && message) {
+    console.log({ email, message });
+    clearLocalStorage();
+    document.querySelector('.feedback-form').reset();
+  }
+});
